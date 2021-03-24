29b350c2879ad117f7af5d9dfcd7ff5281212d5c
Shiv Shankar <sshiv29@berkeley.edu>
2021-03-24 12:59:36 -0700

    setup

---
+++ fake-news-neural-net.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scikitplot as skplt
+
+from tensorflow import keras
+from keras import backend as K
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, Input, RepeatVector
+
+
+
+"""Plots confustion matrix."""
+def plot_cmat(y_test, y_pred):
+    skplt.plot_confusion_matrix(y_test, y_pred)
+    plt.show()
+
+
+""" Basic Neural Network"""
+def baseline_model():
+    model = Sequential()
+    model.add(Dense(256, input_dim=300, activation='relu', kernel_initializer='normal'))
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation='relu', kernel_initializer='normal'))
+    model.add(Dropout(0.5))
+    model.add(Dense(80, activation='relu', kernel_initializer='normal'))
+    model.add(Dense(2, activation='softmax', kernel_initializer='normal'))
+
+    sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
